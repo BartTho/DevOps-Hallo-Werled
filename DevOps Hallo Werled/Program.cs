2134ec75bb184cb86c01645eb8e1ce3b38a31539
bart <bart.thonissen@thomasmore.be>
2020-11-02 11:15:46 +0100

    Add project files.

---
+++ DevOps Hallo Werled/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOps_Hallo_Werled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
